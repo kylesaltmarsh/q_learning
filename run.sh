d1da04c7c47e438bdeaa56695fcba70d403f7cbc
kylesaltmarsh <kyle.saltmarsh@research.uwa.edu.au>
2020-05-08 18:22:39 +0800

    Fixed run.sh file args.

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [[ "$1" = train ]]
 then
-    python frozen_lake.py -is_slippery 0 --path /opt/ml/model/
+    python frozen_lake.py --is_slippery 0 --path /opt/ml/model/
 else
     python frozen_lake.py --mode play
 fi
